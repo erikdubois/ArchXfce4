729dd38db54f07b565d5f4ddfaf28ea87d2c0701
Erik Dubois <erik.dubois@gmail.com>
2017-02-04 13:55:50 +0100

    Comment :  on Sat Feb  4 13:55:50 CET 2017

--- installation/050-install-xfce-core-v1.sh
+++ installation/050-install-xfce-core-v1.sh
@@ -12,45 +12,9 @@ set -e
 ##################################################################################################################
 
 
-# gnome
-
 echo "################################################################"
-echo "gnome and gdm"   
+echo "gdm"   
 echo "################################################################"
-
-package="gnome"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-
-fi
 
 
 package="gdm"
@@ -88,13 +52,53 @@ else
 fi
 
 
+echo "################################################################"
+echo "xfce"   
+echo "################################################################"
+
+
+package="xfce"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+
+fi
+
+
 
 
 echo "################################################################"
-echo "budgie-desktop"   
+echo "xfce-goodies"   
 echo "################################################################"
 
-package="budgie-desktop"
+package="xfce-goodies"
 
 #----------------------------------------------------------------------------------
 
